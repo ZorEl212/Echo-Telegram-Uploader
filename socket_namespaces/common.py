586d1d0b18f8bca688eca4eca90f111ed984bb38
ZorEl212 <yabsirad212@proton.me>
2024-09-10 20:15:34 -0400

    Namespaces: Modify auth logic.
    Take advantage of `always connect` and use emit when necessary when establishing user session .
    
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- socket_namespaces/common.py
+++ socket_namespaces/common.py
@@ -1,19 +1,20 @@
 from socketio import Namespace
 from models import config, sio
 from models.auth import Auth
-
+from socketio.exceptions import ConnectionRefusedError
 class Common(Namespace):
     def on_connect(self, sid, environ, auth):
         print(f"Client {sid} connected")
         if auth is None or 'token' not in auth:
-            return {'message': 'Missing token'}
+            sio.emit('auth_required', {'message': 'Authentication required'}, to=sid, namespace='/daemon')
+            return True
         try:
             payload = Auth.verify_token(auth['token'])
             if not payload:
-                return {'message': 'Invalid token'}
+                raise ConnectionRefusedError({'message': 'Invalid token', 'err_code': 1001})
         except Exception as e:
             print(f"Token verification error: {e}")
-            return {'message': 'Token verification error'}
+            raise ConnectionRefusedError({'message': 'Invalid token', 'err_code': 1001})
 
         return True
 
@@ -22,19 +23,20 @@ class Common(Namespace):
         serverId = data.get('serverId')
 
         if not userId or not serverId:
-            sio.emit('auth_failed', {'message': 'Missing userId or serverId'}, to=sid)
+            sio.emit('auth_failed', {'message': 'Missing userId or serverId', 'err_code': 1002}, to=sid, namespace='/daemon')
             print("Missing userId or serverId")
-            return {'message': 'Missing userId or serverId'}
+            sio.disconnect(sid, namespace='/daemon')
 
         retval = Auth.check_server_details(userId, serverId)
         if not retval:
-            return {'message': 'Invalid server details'}
+            sio.emit('auth_failed', {'message': 'Invalid server details', 'err_code': 1003}, to=sid, namespace='/daemon')
+            print("Invalid server details")
+            sio.disconnect(sid, namespace='/daemon')
+            return
 
         jwe_data = {'userId': userId, 'serverId': serverId}
         token = Auth.create_token(jwe_data, sid)
+        sio.emit('authenticated', {'token': token, 'server': retval}, to=sid, namespace='/daemon')
 
-        sio.emit('authenticated', {'token': token, 'server': retval}, to=sid)
-        return True
-
-    def on_disconnect(self, sid, environ):
+    def on_disconnect(self, sid):
         print(f"Client {sid} disconnected")

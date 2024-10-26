d754de32d1545671ec952bbb1fde624439f4bee5
ZorEl212 <yabsirad212@gmail.com>
2024-10-26 12:33:55 +0300

    Models/Auth: Replace some hardoced messages
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/exception/auth.py
+++ models/exception/auth.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+import resources.strings as strings
 class AuthenticationError(Exception):
     def __init__(self, message, err_code):
         self.message = message
         self.err_code = err_code
 
     def __str__(self):
-        return f"AuthenticationError: {self.message}"
+        return f"{strings.EXC_AUTH_ERROR}: {self.message}"
 

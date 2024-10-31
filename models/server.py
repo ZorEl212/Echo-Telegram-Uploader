75778cb0b11e7dc20397efdadea8012b9445ffe1
ZorEl212 <yabsirad212@gmail.com>
2024-10-31 07:47:44 -0400

    models/server: better handling of server user list
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/server.py
+++ models/server.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python3
-
 from models.base_model import BaseModel
+from typing import List
 
 class Server(BaseModel):
-    userId = ""
-    serverName = ""
-    ipAddress = ""
-    server_users = set()
-    
+    userId: str = ""
+    serverName: str = ""
+    ipAddress: str = ""
+
     @property
     def availableBuilds(self):
         from models import storage
         return storage.all('Build', 'server_id', self.id)
 
-    def add_user(self, userId):
-        if userId not in self.server_users:
-            self.server_users.add(userId)
+    def add_user(self, user_id: str):
+        if not hasattr(self, 'users'):
+            setattr(self, 'users', [user_id])
+        if user_id not in self.users:
+            self.users.append(user_id)
             return True
         return False
